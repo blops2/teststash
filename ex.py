f270a70da1cc2d5ebea0c261326202a3d33aa737
David Thomson <dmt257257@gmail.com>
2018-05-08 13:30:37 +0100

    added example .py file with a single function

---
+++ ex.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+"""
+
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s")
+#logging.disable(logging.CRITICAL)
+
+
+def add(a, b):
+    pass
+
